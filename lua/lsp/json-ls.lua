92757dbc90815498a406ab2bfc635d8cd489216a
ellutionist <impc53802956@163.com>
2023-03-15 17:16:37 +0800

    jsonls

---
+++ lua/lsp/json-ls.lua
@@ -0,0 +1,6 @@
+local common = require "lsp.common"
+
+require 'lspconfig'.jsonls.setup {
+    on_attach = common.on_attach,
+    flags = common.flags
+}
--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -1,5 +1,5 @@
 require "mason".setup()
 require "mason-lspconfig".setup {
-    ensure_installed = { "bashls", "lua_ls", "clangd" },
+    ensure_installed = { "bashls", "lua_ls", "clangd", "jsonls" },
 }
 
