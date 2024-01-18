edd7327b7710824dbff7e5b70b0215c21fbcbbf0
ellutionist <impc53802956@163.com>
2024-01-18 15:07:52 +0800

    bump telescope version

--- lua/plugins/manifest.lua
+++ lua/plugins/manifest.lua
@@ -37,7 +37,7 @@ return {
         },
         {
             "nvim-telescope/telescope.nvim",
-            tag = "0.1.1",
+            tag = "0.1.4",
             -- branch = "master",
             -- or                            , branch = '0.1.x',
             config = require "utils.telescope-setup",
