06546cd58a5647765a8c11beca2420ac701fa9ff
ellutionist <impc53802956@gmail.com>
2024-08-07 15:58:27 +0800

    feat: rust debug

--- lua/dap_module/cpp-debug.lua
+++ lua/dap_module/cpp-debug.lua
@@ -2,28 +2,64 @@ local dap = require "dap"
 
 local codelldb_port = 13000
 dap.adapters.codelldb = {
-	type = "server",
-	port = codelldb_port,
-	executable = {
-		command = "codelldb",
-		args = { "--port", codelldb_port },
-	},
+    type = "server",
+    port = codelldb_port,
+    executable = {
+        command = "codelldb",
+        args = { "--port", codelldb_port },
+    },
 }
 
 dap.configurations.cpp = {
-	{
-		name = "Launch file",
-		type = "codelldb",
-		request = "launch",
-		program = function()
-			return vim.fn.input(
-				"Path to executable: ",
-				vim.fn.getcwd() .. "/",
-				"file"
-			)
-		end,
+    {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+            return vim.fn.input(
+                "Path to executable: ",
+                vim.fn.getcwd() .. "/",
+                "file"
+            )
+        end,
         args = {},
-		cwd = "${workspaceFolder}",
-		stopOnEntry = false,
-	},
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+    },
+}
+dap.configurations.rust = {
+    {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+            return vim.fn.input(
+                "Path to executable: ",
+                vim.fn.getcwd() .. "/",
+                "file"
+            )
+        end,
+        args = {},
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        initCommands = function()
+            -- Find out where to look for the pretty printer Python module
+            local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
+
+            local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+            local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+
+            local commands = {}
+            local file = io.open(commands_file, 'r')
+            if file then
+                for line in file:lines() do
+                    table.insert(commands, line)
+                end
+                file:close()
+            end
+            table.insert(commands, 1, script_import)
+
+            return commands
+        end,
+    },
 }
