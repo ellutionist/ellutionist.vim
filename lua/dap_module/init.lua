21fb0045daf92d48a78b731660f834a91d248474
ellutionist <impc53802956@gmail.com>
2024-10-10 21:21:48 +0800

    improve rust debug

--- lua/dap_module/init.lua
+++ lua/dap_module/init.lua
@@ -1,6 +1,7 @@
 return function()
-	require "dap_module.dap-init"
-	require "dap_module.cpp-debug"
-	require "dap_module.python-debug"
-	require "dap_module.go-debug"
+    require "dap_module.dap-init"
+    require "dap_module.cpp-debug"
+    require "dap_module.rust-debug"
+    require "dap_module.python-debug"
+    require "dap_module.go-debug"
 end
