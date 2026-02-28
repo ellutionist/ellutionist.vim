853a08bd8b66af191db791003d33dab8f3d3a655
ellutionist <impc53802956@gmail.com>
2026-02-28 10:50:26 +0800

    feat: open code

--- lua/plugins/manifest.lua
+++ lua/plugins/manifest.lua
@@ -205,6 +205,15 @@ return {
         }
     },
     ai = {
+        {
+            "NickvanDyke/opencode.nvim",
+            requires = {
+                "folke/snacks.nvim",
+            },
+            config = function()
+                require("opencode").setup()
+            end
+        }
         -- {
         --     "coder/claudecode.nvim",
         --     requires = {
